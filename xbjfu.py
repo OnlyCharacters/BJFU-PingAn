5043d897fbcc6ace1a04f0f861170463f82cb41b
OnlyCharacters <yuxian9630@gmail.com>
2022-09-08 15:37:31 +0800

    update xbjfu.py

--- xbjfu.py
+++ xbjfu.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
@@ -12,35 +13,41 @@ password_u = ""
 opt = webdriver.ChromeOptions()
 opt.add_argument("headless")
 opt.add_argument("no-sandbox")
+opt.add_argument("disable-gpu")
 driver = webdriver.Chrome(options=opt)
 
 driver.get("https://cas.bjfu.edu.cn/")
+
+# 输入账号密码和登录
 try:
-    element = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, '//*[@id="un"]')))
-finally:
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="un"]')))
+except TimeoutException:
+    driver.quit()
+else:
     username = driver.find_element(by="xpath", value='//*[@id="un"]')
     password = driver.find_element(by="xpath", value='//*[@id="pd"]')
     ActionChains(driver).send_keys_to_element(username, username_u).perform()
     ActionChains(driver).send_keys_to_element(password, password_u).perform()
     driver.find_element(by="xpath", value='//*[@id="index_login_btn"]/input').click()
 
-
+# 进入报平安表格
 try:
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="ul_workhall_svsdd"]/li[1]/div[1]/img'))
-    )
-finally:
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ul_workhall_svsdd"]/li[1]/div[1]/img')))
+except TimeoutException:
+    driver.quit()
+else:
     driver.find_element(by="xpath", value='//*[@id="ul_workhall_svsdd"]/li[1]/div[1]/img').click()
     windows = driver.window_handles
     driver.switch_to.window(windows[-1])
 
+# 提交
 try:
-    element = WebDriverWait(driver,155).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="commit"]'))
-    )
-finally:
-    driver.find_element(by="xpath", value='//*[@id="commit"]').click()
-    time.sleep(3)
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="body_0"]/div[1]/div[2]/div[4]/span')))
+except TimeoutException:
     driver.quit()
-
-driver.quit()
+except UnexpectedAlertPresentException:
+    driver.quit()
+else:
+    driver.find_element(by="xpath", value='//*[@id="commit"]').click()
+    time.sleep(2)
+    driver.quit()
