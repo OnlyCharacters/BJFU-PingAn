b8f3be1b3aaca8d41bb1a43902921687ef290079
OnlyCharacters <yuxian9630@gmail.com>
2022-09-22 00:16:52 +0800

    适配新的页面

--- xbjfu.py
+++ xbjfu.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException
+from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException, NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
@@ -16,7 +16,7 @@ opt.add_argument("no-sandbox")
 opt.add_argument("disable-gpu")
 driver = webdriver.Chrome(options=opt)
 
-driver.get("https://cas.bjfu.edu.cn/")
+driver.get("https://x.bjfu.edu.cn/tp_up/view?m=bjfu#act=question/question")
 
 # 输入账号密码和登录
 try:
@@ -31,25 +31,34 @@ else:
     driver.find_element(by="xpath", value='//*[@id="index_login_btn"]/input').click()
 
 # 进入报平安表格
-try:
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ul_workhall_svsdd"]/li[1]/div[1]/img')))
-except TimeoutException:
-    driver.quit()
-else:
-    driver.find_element(by="xpath", value='//*[@id="ul_workhall_svsdd"]/li[1]/div[1]/img').click()
-    windows = driver.window_handles
-    driver.switch_to.window(windows[-1])
+# try:
+#     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ul_workhall_svsdd"]/li[1]/div[1]/img')))
+# except TimeoutException:
+#     driver.quit()
+# else:
+#     driver.find_element(by="xpath", value='//*[@id="ul_workhall_svsdd"]/li[1]/div[1]/img').click()
+#     windows = driver.window_handles
+#     driver.switch_to.window(windows[-1])
 
 # 提交
 try:
-    element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="layui-layer1"]/div')))
-    time.sleep(5)
+    element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="question_btn_query"]')))
 except TimeoutException:
     print("TimeoutException")
     driver.quit()
 except UnexpectedAlertPresentException:
     print("UnexpectedAlertPresentException")
     driver.quit()
+except NoSuchElementException:
+    if EC.text_to_be_present_in_element((By.XPATH, '//*[@id="layui-layer1"]/div[2]'), "今日已报平安！"):
+        print("今日已报平安")
+    else:
+        print("NoSuchElementException")
+    driver.quit()
+except ElementClickInterceptedException:
+    print("今日已报平安")
+    driver.quit()
 else:
-    driver.find_element(by="xpath", value='//*[@id="commit"]').click()
+    time.sleep(1)
+    driver.find_element(by="xpath", value='//*[@id="question_btn_query"]').click()
     driver.quit()
